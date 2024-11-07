82011487ee9ee024d6cd5d073ed35a53bf87749c
ZakHoq <zakariahoque14@gmail.com>
2024-11-07 14:12:18 -0500

    First Commit

---
+++ button_press.js
@@ -0,0 +1,35 @@
+function onButtonPress(){
+    let randomVar= Math.floor(Math.random() * 10);
+switch(randomVar){
+case 0 :
+    alert("Anyone who attempts to generate random numbers by deterministic means is, of course, living in a state of sin -John von Neumann");
+    break;
+case 1:
+    alert("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence - Franz Kafka");
+    break;
+case 2:
+    alert("There are good people who are dealt a bad hand by fate, and bad people who live long, comfortable, privileged lives. A small twist of fate can save or end a life; random chance is a permanent, powerful player in each of our lives, and in human history as well - Jeff Greenfield");
+    break;
+case 3:
+    alert('"Maybe life is random, but I doubt it -Steven Tyler"');
+    break;
+case 4:
+    alert("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination - Sushant Singh Rajput");
+    break;
+case 5:
+    alert("I believe life is an intelligent thing: that things aren't random - Steve Jobs");
+    break;
+case 6:
+    alert("Goals transform a random walk into a chase -Mihaly Csikszentmihalyi");
+    break;
+case 7:
+    alert("I don't believe that if you do good, good things will happen. Everything is completely accidental and random. Sometimes bad things happen to very good people and sometimes good things happen to bad people. But at least if you try to do good things, then you're spending your time doing something worthwhile - Helen Mirren");
+    break;
+case 8:
+    alert("Together we can change the world, just one random act of kindness at a time - Ron Hall");
+    break;
+case 9:
+    alert("Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you - Princess Diana");
+    break;
+}
+}
